dcc9ff906ddecfc67d4460f1327b3169b0ef7e40
Vera Obscura <veraobscura@gmail.com>
2022-06-26 17:04:11 -0400

    feat(qka8frwq): refactored foyer code

--- src/scenes/4_foyer/foyer.tsx
+++ src/scenes/4_foyer/foyer.tsx
@@ -1,4 +1,13 @@
-import { strings } from "./strings";
+import { useAppSelector } from "redux/hooks";
+
+import {
+  SceneContainer,
+  BaseContainer,
+  UIContainer,
+  TextContainer,
+  ImageContainer,
+} from "shared/containers";
+import Inventory from "shared/inventory";
 
 import Base from "./components/base";
 import Floors from "./components/floors";
@@ -11,46 +20,52 @@ import MirrorTable from "./components/mirrorTable";
 import Vase from "./components/vase";
 import Banister from "./components/banister";
 
-import Tile from "gameObjects/tile";
-
-import styles from "./foyer.module.scss";
+import {
+  DATE,
+  PARA_1,
+  PARA_2,
+  PARA_3,
+  PARA_4,
+  PARA_5,
+  PARA_6,
+  PARA_7,
+} from "./foyerNarrative";
 
 const Foyer = () => {
-  return (
-    <div className={styles.foyerContainer}>
-      <section className={styles.foyer}>
-        <div className={styles.baseContainer}>
-          <Base />
-        </div>
-        <div className={styles.uiContainer}>
-          <div className={styles.textContainer}>
-            <p className={styles.date}>{strings.DATE}</p>
-            <p>{strings.PARA_1}</p>
-            <p>{strings.PARA_2}</p>
-            <p>{strings.PARA_3}</p>
-            <p>{strings.PARA_4}</p>
-            <p>{strings.PARA_5}</p>
-            <p>{strings.PARA_6}</p>
-            <p>{strings.PARA_7}</p>
-          </div>
+  const tiles = useAppSelector((state) => state.tiles);
+  const baskets = useAppSelector((state) => state.baskets);
 
-          <div className={styles.inventoryContainer}>
-            <Tile word={"Test"} />
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Floors />
-          <Walls />
-          <Ceiling />
-          <Baseboards />
-          <Door />
-          <Chandelier />
-          <MirrorTable />
-          <Vase />
-          <Banister />
-        </div>
-      </section>
-    </div>
+  return (
+    <SceneContainer>
+      <BaseContainer>
+        <Base />
+      </BaseContainer>
+      <UIContainer>
+        <TextContainer>
+          <DATE />
+          <PARA_1 tiles={tiles.foyer.para1} baskets={baskets.foyer.para1} />
+          <PARA_2 tiles={tiles.foyer.para2} baskets={baskets.foyer.para2} />
+          <PARA_3 tiles={tiles.foyer.para3} baskets={baskets.foyer.para3} />
+          <PARA_4 tiles={tiles.foyer.para4} baskets={baskets.foyer.para4} />
+          <PARA_5 tiles={tiles.foyer.para5} baskets={baskets.foyer.para5} />
+          <PARA_6 tiles={tiles.foyer.para6} baskets={baskets.foyer.para6} />
+          <PARA_7 tiles={tiles.foyer.para7} baskets={baskets.foyer.para7} />
+        </TextContainer>
+
+        <Inventory />
+      </UIContainer>
+      <ImageContainer>
+        <Floors />
+        <Walls />
+        <Ceiling />
+        <Baseboards />
+        <Door />
+        <Chandelier />
+        <MirrorTable />
+        <Vase />
+        <Banister />
+      </ImageContainer>
+    </SceneContainer>
   );
 };
 
---
+++ src/scenes/4_foyer/foyerNarrative.tsx
@@ -0,0 +1,101 @@
+import { useAppSelector } from "redux/hooks";
+
+import { WordType } from "types/word";
+import { BasketType } from "types/basket";
+import { mapBaskets, mapTiles, checkTextBlockComplete } from "helpers/helpers";
+
+import TextBlock from "gameObjects/textBlock";
+
+interface NarrativeProps {
+  tiles: WordType[];
+  baskets: BasketType[];
+}
+
+export const DATE = () => {
+  return (
+    <TextBlock isComplete={false} isDate={true}>
+      August 23, 1991
+    </TextBlock>
+  );
+};
+
+export const PARA_1 = ({ baskets }: NarrativeProps) => {
+  const basketComponents = mapBaskets(baskets, "foyer", "para1");
+
+  return (
+    <TextBlock isComplete={checkTextBlockComplete(basketComponents)}>
+      Hi Lawrence, me again. Sorry for the
+      {basketComponents[0]}. Your funeral was the day before last. I haven't
+      felt like writing
+      {basketComponents[1]}.
+    </TextBlock>
+  );
+};
+
+export const PARA_2 = ({ tiles }: NarrativeProps) => {
+  const inventory = useAppSelector((state) => state.inventory.tiles);
+  const tileComponents = mapTiles(inventory, tiles);
+
+  return (
+    <TextBlock isComplete={false}>
+      I don't know what I'm doing. I{tileComponents[0]}
+      you so much. I just need to talk to you in some way, any way, even through
+      this diary. Life feels so quiet right now, I have so little to do, it
+      seems all I have time to do is think. Think and remember.
+    </TextBlock>
+  );
+};
+
+export const PARA_3 = ({ tiles, baskets }: NarrativeProps) => {
+  return (
+    <TextBlock isComplete={false}>
+      When you got sick, you needed to lie in bed in just the right way, and
+      stopped being able to hold me the way you used to. Over time, I actually
+      forgot what it felt like to fall asleep to the warmth of your breath on my
+      shoulders. I remember things like that now.
+    </TextBlock>
+  );
+};
+
+export const PARA_4 = ({ tiles, baskets }: NarrativeProps) => {
+  return (
+    <TextBlock isComplete={false}>
+      When you came along, I was just getting accustomed to the joy of
+      discovery, the confidence that came with standing on my own two feet. I
+      never expected how much I would enjoy having you sweeping me off them.
+    </TextBlock>
+  );
+};
+
+export const PARA_5 = ({ tiles, baskets }: NarrativeProps) => {
+  return (
+    <TextBlock isComplete={false}>
+      It took a while, but eventually I felt safe enough to tell you what it was
+      like for me growing up. I told you I never wanted to be one of those girls
+      that crossed the foyer of my father's house, barely seeing the sunlight
+      before she was once again cloistered under her husband's roof. You
+      understood completely. I knew I loved you then.
+    </TextBlock>
+  );
+};
+
+export const PARA_6 = ({ tiles, baskets }: NarrativeProps) => {
+  return (
+    <TextBlock isComplete={false}>
+      I surprised my friends when I told you “yes” when you proposed, you know.
+      Again, when you got sick, I surprised everyone at the firm when I turned
+      in my request for an unpaid leave of absence. I was just so determined to
+      nurse you back to health. My mother always called me an impulsive child,
+      someone prone to “breaking everything I touched”. Maybe it would have
+      surprised her that you were getting better.
+    </TextBlock>
+  );
+};
+
+export const PARA_7 = ({ tiles, baskets }: NarrativeProps) => {
+  return (
+    <TextBlock isComplete={false}>
+      I'm just full of surprises, I guess.
+    </TextBlock>
+  );
+};
